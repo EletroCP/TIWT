6d17ae0098930362d713fde18f3e304be222e393
eletrocp <brernolopes@hotmail.com>
2022-10-19 20:16:34 -0300

    :sparkles: feat: componente de "how to play"

---
+++ src/components/HowToPlay.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HowToPlayInfos from '../utils/HowToPlayInfos';
+
+const HowToPlay = () => {
+  console.log(HowToPlayInfos)
+  return (
+    <span aria-label="How To Play">
+      <h1> How To Play WITW? </h1>
+      <p>{ HowToPlayInfos[0].text }</p>
+          {!HowToPlayInfos[0].img ? (
+            <img src={ HowToPlayInfos[0].img } alt="image"/>
+            ) : (
+            null
+          )}
+    </span>
+  )
+};
+
+export default HowToPlay;
