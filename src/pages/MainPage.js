6794b07364420d66f9403adce3e4de0067edaa33
eletrocp <brernolopes@hotmail.com>
2022-10-19 19:03:44 -0300

    :sparkles: feat: criado componente principal

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const MainPage = () => {
   return(
     <div>
-      <p>
-        WITW
-      </p>
+      <Header />
+      <input type="text" placeholder="Your name" />
+      <label>
+        Select game Type:
+        <button>One Word</button>
+        <button>Two Words</button>
+        <button>Three Words</button>
+      </label>
+      <button>Start</button>
+      <button>How To Play</button>
     </div>
   )
 }
