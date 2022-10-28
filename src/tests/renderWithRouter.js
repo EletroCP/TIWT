4536b0fb771f1514778e379d6af64d8efe4ce0f9
eletrocp <brernolopes@hotmail.com>
2022-10-28 07:45:38 -0300

    :recycle: refactor: removendo provider do router

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -3,15 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-import ContextProvider from '../context/Context';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
+
   return ({
     ...render(
-      <ContextProvider>
-        <Router history={ history }>{component}</Router>
-      </ContextProvider>
+      <Router history={ history } >{component}</Router>
     ), history,
   });
 };
