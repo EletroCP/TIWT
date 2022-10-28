a40804410094a3cc69eeeb9996f2c8e6a337f0f9
Breno Laskavski Lopes <94480224+EletroCP@users.noreply.github.com>
2022-10-28 16:11:03 -0300

    Revert "Main html logic"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Router from './components/Router';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App" id="app">
-        <Router />
+      <div className="App">
+        <p>WITW</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import ContextProvider from './context/Context';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
-reportWebVitals();
